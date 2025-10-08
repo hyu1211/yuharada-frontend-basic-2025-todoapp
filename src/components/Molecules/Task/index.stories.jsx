96190eff576e2573f0c8687f7838502a322c9b9c
yuharada <you.h12112002@gmail.com>
2025-10-08 16:27:55 +0900

    add: Molecules/Task

--- src/components/Molecules/Task/index.stories.jsx
+++ src/components/Molecules/Task/index.stories.jsx
@@ -1,13 +1,16 @@
 import { Task } from "./index";
 
-export default {component: Task};
+export default { component: Task };
 
 export const Default = {
-    args: {
-        taskName: "タスクを入力",
-        defaultIsEditing: false,
-        onTaskComplete: (taskName) => {
-        console.log("task name changed: <" + taskName +">")
-        },
+  args: {
+    taskName: "タスクを入力",
+    defaultIsEditing: false,
+    onTaskNameChange: (taskName) => {
+      console.log("task name changed: <" + taskName + ">");
     },
+    onTaskComplete: () => {
+        console.log("task complete");
+    }
+  },
 };
